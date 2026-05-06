4d42bea1e7a3b453b3659914f836a5ca2af20722
kkosinovaa <kkatrusaptasechka@gmail.com>
2026-05-06 23:29:04 +0300

    додала файл InventoryTable.jsx та поміняла місце знаходження ConfirmModal.jsx

---
+++ src/components/inventory/InventoryTable.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function InventoryTable({ items, onDelete }) {
+    return (
+        <table className="admin-table">
+            <thead>
+            <tr>
+                <th>Фото</th>
+                <th>Назва</th>
+                <th>Опис</th>
+                <th>Дії</th>
+            </tr>
+            </thead>
+            <tbody>
+            {items.map((item) => (
+                <tr key={item.id}>
+                    <td>
+                        {item.imageUrl ? <img src={item.imageUrl} alt={item.name} width="50" /> : 'Немає фото'}
+                    </td>
+                    <td><strong>{item.name}</strong></td>
+                    <td>{item.description}</td>
+                    <td>
+                        <Link to={`/details/${item.id}`} style={{ marginRight: '10px' }}>👁️ Перегляд</Link>
+                        <Link to={`/edit/${item.id}`} style={{ marginRight: '10px' }}>✏️ Редаг.</Link>
+                        <button onClick={() => onDelete(item)}>🗑️ Видал.</button>
+                    </td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+    );
+}
