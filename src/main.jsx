9d38492c3d6e8067a2c5a1ae362bbfeda393b44e
kkosinovaa <kkatrusaptasechka@gmail.com>
2026-05-13 18:08:35 +0300

    fix: додано обгортку контексту для хука useInventory, щоб уникнути помилки null

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import { InventoryProvider } from './store/InventoryContext';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import { InventoryProvider } from './store/InventoryContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <InventoryProvider>
-            <App />
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
         </InventoryProvider>
     </React.StrictMode>
 );
